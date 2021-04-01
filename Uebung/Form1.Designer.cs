eea547417a07bbd4cfa51cd7c0551fb1a326527d
morfeus3009 <66361047+morfeus3009@users.noreply.github.com>
2021-04-01 02:27:10 +0200

    Projektdateien hinzufügen.

---
+++ Uebung/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace Uebung
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInOut = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnRes = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInOut
+            // 
+            this.txtInOut.Location = new System.Drawing.Point(12, 30);
+            this.txtInOut.Name = "txtInOut";
+            this.txtInOut.Size = new System.Drawing.Size(249, 23);
+            this.txtInOut.TabIndex = 0;
+            this.txtInOut.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtInOut.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtInOut_KeyDown);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAdd.Location = new System.Drawing.Point(12, 132);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(43, 41);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSub.Location = new System.Drawing.Point(115, 132);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(43, 41);
+            this.btnSub.TabIndex = 2;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // btnRes
+            // 
+            this.btnRes.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnRes.Location = new System.Drawing.Point(218, 132);
+            this.btnRes.Name = "btnRes";
+            this.btnRes.Size = new System.Drawing.Size(43, 41);
+            this.btnRes.TabIndex = 3;
+            this.btnRes.Text = "=";
+            this.btnRes.UseVisualStyleBackColor = true;
+            this.btnRes.Click += new System.EventHandler(this.btnRes_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnClear.Location = new System.Drawing.Point(12, 72);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(43, 41);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "C";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 185);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRes);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtInOut);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInOut;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnRes;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Uebung/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uebung
+{
+    
+    public partial class Form1 : Form
+    {
+        private int number1 = 0;
+        private int number2 = 0;
+        private string res = "ERROR";
+        private string operation = null;
+        private bool result = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            numbersAdd();
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            numberSub();
+        }
+
+        private void numbersAdd()
+        {
+            number1 = int.Parse(txtInOut.Text);
+            operation = "+";
+            txtInOut.Text = string.Empty;
+            txtInOut.Focus();
+        }
+
+        private void numberSub()
+        {
+            number1 = int.Parse(txtInOut.Text);
+            operation = "-";
+            txtInOut.Text = "";
+            txtInOut.Focus();
+        }
+
+        private void numberRes()
+        {
+            number2 = int.Parse(txtInOut.Text);
+            switch (operation)
+            {
+                case "+":
+                    res = (number1 + number2).ToString();
+                    break;
+                case "-":
+                    res = (number1 - number2).ToString();
+                    break;
+                default:
+                    break;
+            }
+            //txtInOut.Text = res;
+            result = true;
+        }
+        private void btnRes_Click(object sender, EventArgs e)
+        {
+            numberRes();
+        }
+
+
+        private void txtInOut_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Add)
+            {
+                numbersAdd();
+            }
+            if (e.KeyCode == Keys.Subtract)
+            {
+                numberSub();
+            }
+            if (e.KeyCode == Keys.Enter)
+                numberRes();
+            if (e.KeyCode == Keys.Delete)
+                clearAll();
+            txtInOut.Text = !result ? string.Empty : res;
+            
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clearAll();
+        }
+
+        private void clearAll()
+        {
+            number1 = 0;
+            number2 = 0;
+            operation = null;
+            txtInOut.Text = string.Empty;
+            res = string.Empty;
+        }
+    }
+}
